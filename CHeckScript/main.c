31854e3ce1f27794972700c7ea114e170e8ca1cd
Mashpoe <Mashpoe@Mashpoes-MacBook-Pro.local>
2019-02-26 16:44:07 -0700

    Initial Commit

---
+++ CHeckScript/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CHeckScript
+//
+//  Created by Mashpoe on 2/26/19.
+//  Copyright © 2019 Mashpoe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
