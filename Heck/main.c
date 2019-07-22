192bec82084e56d310de88005c852587cf930bc1
Mashpoe (Jacob DeHart) <mashpoesemail@gmail.com>
2019-07-22 01:49:22 -0600

    Initial Commit

---
+++ Heck/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Heck
+//
+//  Created by Mashpoe on 7/22/19.
+//  Copyright © 2019 Mashpoe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
