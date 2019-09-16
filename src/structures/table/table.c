b4657cf0b98cb5f0d24a903ee9350b65ea9b0122
Mashpoe (Jacob DeHart) <mashpoesemail@gmail.com>
2019-09-16 00:07:36 -0600

    remove old hashmap

---
+++ src/structures/table/table.c
@@ -0,0 +1,19 @@
+//
+//  table.c
+//  Heck
+//
+//  Created by Mashpoe on 9/15/19.
+//
+
+#include "table.h"
+
+inline uint32_t hash_data(const void* data, size_t size) {
+	// FNV-1a hashing algorithm, the shortest decent hash function, apparently
+	uint32_t hash = 2166136261u;
+	for (size_t i = 0; i < size; i++) {
+		hash ^= ((const char*)data)[i];
+		hash *= 16777619;
+		i++;
+	}
+	return hash;
+}
