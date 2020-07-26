6648ea810c3efb4712ab7909b3573fae1fd5e5ca
Mashpoe <mashpoesemail@gmail.com>
2020-07-26 00:26:47 +0000

    added tests

--- src/main.c
+++ src/main.c
@@ -27,14 +27,30 @@ func run() {
 int main(int argc, const char * argv[]) {
 	// insert code here...
 	
-	if (argc == 2 && strcmp(argv[1], "--version") == 0) {
-		printf("0.0.1\n");
-		return 0;
-	}
+  const char* input_path = NULL;
+  const char* output_path = NULL;
+
+  if (argc < 2) {
+    printf("error: expected an argument\n");
+    return 1;
+  } else if (argc == 2) {
+
+    if (strcmp(argv[1], "--version") == 0) {
+      printf("0.0.1\n");
+      return 0;
+    } else {
+      input_path = argv[1];
+    }
+
+	} else {
+    // ik this is the opposite order gcc does
+    input_path = argv[1];
+    output_path = argv[2];
+  }
 	
 	clock_t begin = clock();
 
-	FILE* f = fopen("resolve_test2.heck", "rb");
+	FILE* f = fopen(input_path, "rb");
 
 	if (f) {
 
@@ -44,15 +60,16 @@ int main(int argc, const char * argv[]) {
 		//heck_print_tokens(c);
 		heck_parse(c); // parsing includes resolving for now, since it completes the syntax tree
 		
-		heck_print_tree(c);
+		//heck_print_tree(c);
 		//printf("done.\n");
 		//heck_compile(c);
 		//printf("press ENTER to continue...");
 		//getchar();
 		heck_free(c);
+
+	  fclose(f);
 	}
 
-	fclose(f);
 
 	clock_t end = clock();
 	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
